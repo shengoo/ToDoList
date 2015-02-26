6982411e319ef0b97280676bb4e9c3dd33d458a8
Qing Sheng <shengoo@hotmail.com>
2015-02-26 16:00:29 +0800

    added new controllers

---
+++ AddToDoItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoItemViewController.swift
+//  ToDoList
+//
+//  Created by sheng qing on 15/2/26.
+//  Copyright (c) 2015年 sheng qing. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
